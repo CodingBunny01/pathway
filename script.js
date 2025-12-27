44c8e3e4912acfe17f620fd02340b399fe3ba5dc
CodingBunny01 <90410423+CodingBunny01@users.noreply.github.com>
2025-12-27 18:48:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-window.addEventListener("scroll", () => {
-  document.querySelectorAll(".image").forEach(img => {
-    img.style.opacity = "1";
+const images = document.querySelectorAll('.image');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
   });
 });
+
+images.forEach(img => observer.observe(img));
